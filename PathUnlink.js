59aac2e050c72a40b9e4860f43c67e2c3cfa875f
Colten Jackson <jazzyjackson@gmail.com>
2018-10-25 00:49:36 -0500

    Updated for Transflect v0.6+ compatibility

--- PathUnlink.js
+++ PathUnlink.js
@@ -6,17 +6,15 @@ let fs = require('fs')
  * no need to _open(source) a stream here, source is available as this.source
  */
 module.exports = class PathUnlink extends Transflect {
-    constructor(options){
-        super(options)
-    }
+    constructor(){ super() }
 
     /**
      * set statusCode 204 finished delete, no content to return
      * if unlink calls done with an error, PathUnlink emits error,
      * ServerFailSoft writes error object to client and overwrites statusCode
      */
-    _flush(done){
-        this.statusCode = 204
+    _end(done){
+        this.pipes.statusCode = 204
         fs.unlink(this.source.pathname, done)
     }
 }
